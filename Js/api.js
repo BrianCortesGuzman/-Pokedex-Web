fa5f9d2e6614358e6ff2a51bdf83d83a8e838280
BrianCortesGuzman <BrianDanielProgramer@hotmail.com>
2023-08-21 15:13:59 -0500

    A1

---
+++ Js/api.js
@@ -0,0 +1,10 @@
+
+const buscar2 = document.getElementById("buscar2");
+const buscar = document.getElementById("buscar");
+
+buscar.addEventListener("click", () => {
+const nombre = buscar2.value;
+
+
+
+});
